1b11abc3fe702f2e805e1b5da3c4353ff462abd3
Veli-Matti Laamala <44024374+Vekkumasa@users.noreply.github.com>
2021-12-08 14:01:29 +0200

    Delete compile.sh

--- compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gfortran -c main.f90 walkers.f90 arrayHandler.f90 walkerSimulation.f90
-gfortran main.o walkers.o arrayHandler.o walkerSimulation.o
-./a.exe $1 $2 $3 $4 $5
-rm a.exe main.o walkers.o arrayHandler.o walkerSimulation.o
